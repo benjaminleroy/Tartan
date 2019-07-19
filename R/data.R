98abb28d9a479836158a018ab43a01e7aa055ab4
benjaminleroy <benjaminleroyusa@gmail.com>
2019-07-19 15:21:30 -0400

    added data and some documentation

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Tartans Dataset
+#'
+#' A data frame with information about different Tartans in Scotland.
+#'
+#' @docType data
+#'
+#' @usage data(tartans)
+#'
+#' @keywords datasets
+#'
+#' @source Scraped from the \href{www.tartanregister.gov.uk}{Tartan Register},
+#' 14 July 2019
+#'
+"tartans"
