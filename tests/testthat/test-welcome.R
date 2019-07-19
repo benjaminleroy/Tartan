66560e64bf58ba197aa61fffff8ba354960d0fb4
benjaminleroy <benjaminleroyusa@gmail.com>
2019-07-19 15:17:12 -0400

    corrected test function

--- tests/testthat/test-welcome.R
+++ tests/testthat/test-welcome.R
@@ -3,5 +3,5 @@ context("test welcome from scotty")
 test_that("calling scotty doesn't return anything", {
   expect_equal(call_scotty_demo("Oski Bear"), NULL)
   expect_equal(2 * 2, 4)
-  expect_equal(factorial(4), 25)
+  expect_equal(factorial(4), 24)
 })
